3cd7d49fb21d4d3cfe56f8ab145761c6197c63a9
João Salgado <jgs1884@gmail.com>
2018-03-03 17:06:32 -0300

    Trying deploy 5

--- src/main/resources/db/migration/V03__create_accounting_entries.sql
+++ src/main/resources/db/migration/V03__create_accounting_entries.sql
@@ -18,10 +18,11 @@ INSERT INTO entry (description, due_date, date_payment, value, observation, type
 INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('Top Club', '2017-06-10', null, 120, null, 'RECEITA', 3, 3);
 INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('CEMIG', '2017-02-10', '2017-02-10', 110.44, 'Geração', 'RECEITA', 3, 4);
 INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('DMAE', '2017-06-10', null, 200.30, null, 'DESPESA', 3, 5);
-INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('Bahamas', '2017-06-10', null, 500, null, 'RECEITA', 1, 7);
-INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('Top Club', '2017-03-10', '2017-03-10', 400.32, null, 'DESPESA', 4, 8);
-INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('Despachante', '2017-06-10', null, 123.64, 'Multas', 'DESPESA', 3, 9);
-INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('Pneus', '2017-04-10', '2017-04-10', 665.33, null, 'RECEITA', 5, 10);
+INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('Extra', '2017-03-10', '2017-03-10', 1010.32, null, 'RECEITA', 4, 5);
+INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('Bahamas', '2017-06-10', null, 500, null, 'RECEITA', 1, 4);
+INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('Top Club', '2017-03-10', '2017-03-10', 400.32, null, 'DESPESA', 4, 3);
+INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('Despachante', '2017-06-10', null, 123.64, 'Multas', 'DESPESA', 3, 2);
+INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('Pneus', '2017-04-10', '2017-04-10', 665.33, null, 'RECEITA', 5, 1);
 INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('Café', '2017-06-10', null, 8.32, null, 'DESPESA', 1, 5);
 INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('Eletrônicos', '2017-04-10', '2017-04-10', 2100.32, null, 'DESPESA', 5, 4);
 INSERT INTO entry (description, due_date, date_payment, value, observation, type, id_category, id_person) values ('Instrumentos', '2017-06-10', null, 1040.32, null, 'DESPESA', 4, 3);
